b2c80d21f30e0cdfad55ead7273a5ff2018f9245
Fabricio Felix <felixfabricio115@gmail.com>
2024-08-08 09:31:40 -0300

    internal autenticador de senha geral

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,3 +1,5 @@
 ﻿using bytebank_ATENDIMENTO.bytbank.Atendimento;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 new ByteBankAtendimento().AtendimentoCliente();
+
