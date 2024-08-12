3c380e969a37f05e78b8bab3fd5905d9218b6cf5
Fabricio Felix <felixfabricio115@gmail.com>
2024-08-12 14:02:15 -0300

    criacao de uma dll e garador de chave pix

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,26 +1,36 @@
 ﻿using bytebank.Modelos.ADM.Funcionarios;
 using bytebank_ATENDIMENTO.bytbank.Atendimento;
+using bytebank_GeradorChavePix;
 using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 
 
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+//new ByteBankAtendimento().AtendimentoCliente();
 
-public class Estagiario : Funcionario
+//public class Estagiario : Funcionario
+//{
+//    public Estagiario(double salario, string cpf) : base(salario, cpf)
+//    {
+
+//    }
+
+//    public override void AumentarSalario()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    protected override double getBonificacao()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+
+Console.WriteLine(GeradorPix.GetChavePix());
+
+var listaDeChaves = GeradorPix.GetChavePix(10);
+
+foreach (var chave in listaDeChaves)
 {
-    public Estagiario(double salario, string cpf) : base(salario, cpf)
-    {
-
-    }
-
-    public override void AumentarSalario()
-    {
-        throw new NotImplementedException();
-    }
-
-    protected override double getBonificacao()
-    {
-        throw new NotImplementedException();
-    }
+    Console.WriteLine(chave);
 }
